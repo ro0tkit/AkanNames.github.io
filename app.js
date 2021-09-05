257d4ef1a9a7d9f85b26a469f178c58d9f274fe2
Luther <lutherking30@gmail.com>
2021-09-05 15:48:36 +0300

    Add variables

---
+++ app.js
@@ -0,0 +1,30 @@
+/*
+*/
+
+
+const checkAkan = (e) => {
+    e.preventDefault();
+    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    const femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+    const frm = document.querySelector("#frmAkans");
+    const fd = new FormData(frm);
+    const gender = fd.get("gender");
+    const dob = fd.get("dob");
+
+    const d = new Date(dob).getDay()
+    if (gender == "male"){
+        console.log("Your Akan name is "+maleAkanNames)
+    }else{
+        console.log("Your Akan name is "+maleAkanNames)
+    }
+  
+}
+
+
+
+
+
+
+document.querySelector("#frmAkans").addEventListener("submit", checkAkan);
